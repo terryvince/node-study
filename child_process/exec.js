752b0e913f21f7a61dbeb636b1e8d32d84fd7828
hezhongzheng <2931361912@qq.com>
2023-12-19 15:47:11 +0800

    update

--- child_process/exec.js
+++ child_process/exec.js
@@ -19,28 +19,28 @@ exec('git add .',(err,stdout,stderr)=>{
  });
 
 //执行程序
-const { spawn } = require('child_process');
-const bat = spawn('cmd.exe', ['/c', 'my.bat']);
+// const { spawn } = require('child_process');
+// const bat = spawn('cmd.exe', ['/c', 'my.bat']);
 
-bat.stdout.on('data', (data) => {
-    console.log(data.toString());
-});
+// bat.stdout.on('data', (data) => {
+//     console.log(data.toString());
+// });
 
-bat.stderr.on('data', (data) => {
-    console.log(data.toString());
-});
+// bat.stderr.on('data', (data) => {
+//     console.log(data.toString());
+// });
 
-bat.on('exit', (code) => {
-    console.log(`子进程退出码：${code}`);
-});
+// bat.on('exit', (code) => {
+//     console.log(`子进程退出码：${code}`);
+// });
 
-//执行node程序专属
-const cp = require('child_process');
-const n = cp.fork(`${__dirname}/sub.js`);
+// //执行node程序专属
+// const cp = require('child_process');
+// const n = cp.fork(`${__dirname}/sub.js`);
 
-n.on('message', (m) => {
-    console.log('父进程收到消息', m);
-});
+// n.on('message', (m) => {
+//     console.log('父进程收到消息', m);
+// });
 
 // 使子进程输出: 子进程收到消息 { hello: 'world' }
-n.send({ hello: 'world' });
+// n.send({ hello: 'world' });
