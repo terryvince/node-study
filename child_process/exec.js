32b3b089eb441e668d71c368730f8de8fb29fc2d
hezhongzheng <2931361912@qq.com>
2023-12-19 15:49:53 +0800

    update

--- child_process/exec.js
+++ child_process/exec.js
@@ -11,7 +11,7 @@ const {exec} = require('child_process');
 
 exec('git add . && git commit -m "update"',(err,stdout,stderr)=>{
     if(err){
-     console.error(`执行出错: ${err}`);
+     console.error(`执行出错: ${err}${stderr}`);
      return;
     }
      console.log(`stdout: ${stdout}`);
